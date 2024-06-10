37556559a75fcddd822d8ac093bcd9cd33dce8ce
Josh Taylor <thie.common.coder@gmail.com>
2024-06-09 21:54:42 -0700

    github actions workflow for ghp and index update

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Hello from gh-pages!
+        Hello from GitHub Actions Deployment!
       </p>
     </>
   )
